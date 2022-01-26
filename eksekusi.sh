4eef3789ef246151144ede54a440686ef3c8beb0
aranggitoar <aranggi.josef@gmail.com>
2022-01-26 21:48:08 +0700

    Logistik: menciptakan satu file untuk eksekusi semua kode lain

---
+++ eksekusi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "------------------------------"
+echo "Mengunduh file-file USFM TSI yang sudah siap dibaca ..."
+echo "------------------------------"
+./ambil-sumber.sh
+echo "------------------------------"
+echo "Selesai mengunduh."
+echo "------------------------------"
+echo "Merapikan nama file USFM TSI ..."
+echo "------------------------------"
+./sesuaikan-nama-file.sh
+echo "------------------------------"
+echo "Selesai merapikan."
+echo "------------------------------"
+echo "Membagi isi dari file-file USFM TSI sesuai keperluan ..."
+echo "------------------------------"
+./bagi-sumber.sh
+echo "------------------------------"
+echo "Selesai membagi."
+echo "------------------------------"
+echo "Semua file USFM siap digunakan dalam direktori 'AlkitabKita' dalam 'usfm-hasil'."
+echo "------------------------------"
